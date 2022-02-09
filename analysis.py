d87ef7083c6f2d103e332ad58fdbece5714c3240
Mark Zurbruegg <39518170+Betaglutamate@users.noreply.github.com>
2022-02-09 11:16:48 +0000

    attempting image segmentation

--- analysis.py
+++ analysis.py
@@ -25,6 +25,7 @@ class Analysis():
                 subcell_dict[str(row[1])] = coord
         #load coordinates
         #crop
+        enumerator = 0
 
         for k, v in subcell_dict.items():
             x1, y1, x2, y2 = subcell_dict[k]
@@ -33,13 +34,59 @@ class Analysis():
             y1 = int(float(y1))
             y2 = int(float(y2))
 
-            cell_save_path = os.path.join(self.path, f"cell_{k}")
+            enumerator = enumerator + 1
+
+            cell_save_path = os.path.join(self.path, f"cell_{enumerator}")
 
             Path(cell_save_path).mkdir(parents=True, exist_ok=True)
 
             for num, image in enumerate(self.img_collection):
                 cropped=image[y1:y2, x1:x2]
-                ski.io.imsave(os.path.join(cell_save_path, f"cell_{k}_{num}.png"), cropped)
+                ski.io.imsave(os.path.join(cell_save_path, f"cell_{enumerator}_id{k}_{num}.png"), cropped)
                 
 
 
+test = ski.io.imread("test_images/cell_1/cell_1_id3_2.png")
+
+
+
+import matplotlib.pyplot as plt
+from skimage.filters import try_all_threshold
+import numpy as np
+
+
+fig, ax = try_all_threshold(invert, figsize=(10, 8), verbose=False)
+plt.show()
+
+test_8bit = ski.img_as_ubyte(test)
+
+plt.hist(invert.ravel(), bins = 20)
+
+
+invert = np.invert(test_8bit)
+
+thresh = ski.filters.threshold_mean(invert)
+binary = invert > thresh
+
+plt.imshow(binary)
+
+
+
+edges1 = ski.feature.canny(binary)
+edges2 = ski.feature.canny(binary, sigma=3)
+
+fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(8, 3))
+
+ax[0].imshow(test, cmap='gray')
+ax[0].set_title('noisy image', fontsize=20)
+
+ax[1].imshow(edges1, cmap='gray')
+ax[1].set_title(r'Canny filter, $\sigma=1$', fontsize=20)
+
+ax[2].imshow(edges2, cmap='gray')
+ax[2].set_title(r'Canny filter, $\sigma=3$', fontsize=20)
+
+
+label_im = ski.measure.label(edges2)
+
+clusters = ski.measure.regionprops(label_im, invert)
