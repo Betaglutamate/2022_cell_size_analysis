d59ae67d88473218144c17bbdfce35063b694b25
Mark Zurbruegg <39518170+Betaglutamate@users.noreply.github.com>
2023-06-14 16:22:38 +0100

    remove icon

--- main_app.py
+++ main_app.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     root.geometry("1200x800")
     app = App(root)
     root.title("Cell Analyser")
-    root.tk.call('wm', 'iconphoto', root._w,
-                 tk.PhotoImage(file='bacteria-icon.png'))
+    # root.tk.call('wm', 'iconphoto', root._w,
+    #              tk.PhotoImage(file='bacteria-icon.png'))
     root.mainloop()
